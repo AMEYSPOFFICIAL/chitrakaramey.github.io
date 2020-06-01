41cf148e429435c9fe95af9e4b3af4d7978a951b
AMEYSPOFFICIAL <65562652+AMEYSPOFFICIAL@users.noreply.github.com>
2020-06-01 16:47:57 +0530

    Update contact-form-handler.php

--- css/contact-form-handler.php
+++ css/contact-form-handler.php
@@ -1,25 +1,20 @@
 <?php
-   $name = $_POST['name'];
-   $visitor_email = $_POST['email'];
-   $message = $_POST['message'];
+// variable setting
+$Name = $_REQUEST['Name'];
+$Email = $_REQUEST['Email'];
+$Message = $_REQUEST['Message'];
 
-   $email_from = 'ameypandit264@gmail.com';
-   $email_subject = "New Form Sumission";
-   $email_body = "User Name: $name.\n".
-                   "User Email; $visitor_email.\n".
-                   "User Message: $message.n\";
+//check input fields
+if (empty($Name) || empty($Email) || empty($Message)) 
+{
+	echo "Please Fill All Details";
+}
+else
+{
+	mail("ameysathaye.pc@gmail.com", "Website Message", $message , "From: $Name <$Email>");
+	echo "<script type='text/javascript'>alert('Your message Sent Successfully');
+	window.history.log(-1);
 
-
-   $to = "ameysathaye.pc@gmail.com";
-
-   $headers = "From: $email_from \r\n";
-
-   $headers .= "Reply-To: $visitor_email \r\n";
-
-   mail($to,$email_subject,$email_body,$headers);
-
-   header("Location: page2.html");
-
-
-
+	</script>'';
+}
 ?>
